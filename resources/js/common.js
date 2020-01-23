584bebafd48b653ce2aaaa2eb3ce2e69379ac6ab
Hyungjin.Lee <hyungjin.lee@emotion.co.kr>
2020-01-23 14:07:21 +0900

    ajax connect success!

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,10 +1,32 @@
-var userList = new Vue({
-    el: '#app',
-    data: {
-        postId: '안녕하세요',
-        postType: ''
-    }
-    mounted: {
-        
-    }
-})
+// new Vue ({
+//     el: '#app',
+//     data: {
+//         posts: []
+//     },
+//     created() {
+//         fetch('../data/list.json')
+//         .then((response) => {
+//             if(response.ok) {
+//                 return response.json();
+//             }
+//             throw new Error('Network response was not ok!');
+//         })
+//         .then((json) => {
+//             this.posts.push({
+//                 postId: json.postId,
+//                 postType: json.postType,
+//                 userId: json.user.userId
+//             });
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//         });
+//     }
+// });
+
+// new Vue = ({
+//     el: '#todos',
+//     data: {
+//         template: '<td>{{ postId }}</td>'
+//     }
+// });
